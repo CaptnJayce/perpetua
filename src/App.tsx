41bd818db7e8f5c4dcfc98555abd925b01f106f7
Casey <captn.dev@protonmail.com>
2026-04-30 18:36:03 +0100

    feat: wire game tick loop into App on mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.css";
+
+import { useEffect } from "react";
+import { useGameStore } from "./store";
+
 import Resources from "./components/Resources";
 import Dialogue from "./components/Dialogue";
 import Actions from "./components/Actions";
 
 export default function App() {
+    useEffect(() => {
+        const TICK_MS = 100; // 10 ticks/second
+        const delta = TICK_MS / 1000;
+
+        const interval = setInterval(() => {
+            useGameStore.getState().tick(delta);
+        }, TICK_MS);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="main">
             <Resources />
